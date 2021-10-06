2b2ec5a418cb22147d3c8db719f97c47d164b8ab
adnanesaber15 <adnanesaber15@gmail.com>
2021-10-06 23:33:47 +0100

    deploy tests

--- VBE/settings.py
+++ VBE/settings.py
@@ -125,7 +125,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = (
