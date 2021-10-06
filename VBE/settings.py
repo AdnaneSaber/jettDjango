6a2d3e8b0b08044c4960f42d30a831c0dd0f842f
adnanesaber15 <adnanesaber15@gmail.com>
2021-10-06 23:42:28 +0100

    deploy tests

--- VBE/settings.py
+++ VBE/settings.py
@@ -133,7 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #     os.path.join(BASE_DIR, 'static'),
 # )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
 ]
